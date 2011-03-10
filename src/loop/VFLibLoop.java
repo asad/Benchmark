44cbe89053ab0c72cdae61d0c3ef67f5e5a5adec
Syed Asad Rahman <s9asad@gmail.com>
2011-03-10 17:08:44 +0000

    test case adapeted

--- src/loop/VFLibLoop.java
+++ src/loop/VFLibLoop.java
@@ -1,7 +1,8 @@
 package loop;
 
 import org.openscience.cdk.interfaces.IMolecule;
-import vf2.old.VFMapper;
+import vf2.AtomMapping;
+import vf2.VF2;
 
 public class VFLibLoop extends AbstractSubgraphIsomorphismLoop
         implements TimedSubgraphIsomorphismLoop {
@@ -13,14 +14,14 @@ public class VFLibLoop extends AbstractSubgraphIsomorphismLoop
 
     @Override
     public void run(IMolecule query, IMolecule target) {
-//        VF2 matcher = new VF2();
-//        AtomMapping mapping = matcher.isomorphism(query, target);
-//        if (!mapping.isEmpty()) {
-//            numberOfResults++;
-//        }
-        VFMapper matcher = new VFMapper(query);
-        if (matcher.hasMap(target)) {
+        VF2 matcher = new VF2();
+        AtomMapping mapping = matcher.isomorphism(query, target);
+        if (!mapping.isEmpty()) {
             numberOfResults++;
         }
+//        VFMapper matcher = new VFMapper(query);
+//        if (matcher.hasMap(target)) {
+//            numberOfResults++;
+//        }
     }
 }
