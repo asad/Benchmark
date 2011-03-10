f4e87f68f4e55bd9be826cac97ca081aa3657152
Syed Asad Rahman <s9asad@gmail.com>
2011-03-10 12:06:14 +0000

    coverage test

--- src/test/TestVF2.java
+++ src/test/TestVF2.java
@@ -1,25 +1,14 @@
 package test;
 
-import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.exception.InvalidSmilesException;
-import org.openscience.cdk.interfaces.IAtomContainer;
-import org.openscience.cdk.smiles.SmilesParser;
-import vf2.AtomMapping;
-import vf2.VF2;
 
 public class TestVF2 {
 
-    public static void main(String[] args) throws InvalidSmilesException {
-        SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
-        IAtomContainer query = sp.parseSmiles("CCOC(=O)c1[nH]c2ccc(C)cc2(c1(N))");
-        IAtomContainer target = sp.parseSmiles("CCOC(=O)c2[nH]c1ccc(C)cc1c2(N=CN(CC)CC)");
-////        IAtomContainer target = sp.parseSmiles("CCOC(=O)c1[nH]c2ccc(C)cc2(c1(N))");
-//        IAtomContainer query = sp.parseSmiles("CCO");
-//        IAtomContainer target = sp.parseSmiles("CCOCN");
-        if (query.getAtomCount() <= target.getAtomCount()) {
-            VF2 matcher = new VF2();
-            AtomMapping mapping = matcher.isomorphism(query, target);
-            System.out.println("mapping " + mapping);
-        }
+    public static void main(String[] args) throws InvalidSmilesException, CDKException {
+        TestVF2Coverage vfTest = new TestVF2Coverage();
+        vfTest.TestCovergare();
+        PermutationTest pt = new PermutationTest();
+        pt.cyclopentane();
     }
 }
---
+++ src/test/TestVF2Coverage.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.exception.InvalidSmilesException;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.smiles.SmilesParser;
+import vf2.AtomMapping;
+import vf2.VF2;
+
+/**
+ *
+ * @author Asad
+ */
+public class TestVF2Coverage {
+
+    String[] queries = {"c1ccccc1C(N)C(O)=O",
+        "CC(C)OCC(C)=C",
+        "Nc1ccc(CC)cc1",
+        "O=c1[nH]c(=O)c2ccccc12",
+        "O=C4C=C3CCC2C1CCCC1CCC2C3CC4",
+        "CC2(C)CN1C(=O)C(N)C1S2",
+        "OC1CCCCOCC1"};
+
+    private List<IAtomContainer> readSMILES() {
+        List<IAtomContainer> list = new ArrayList<IAtomContainer>();
+
+        return list;
+    }
+
+    @Test
+    public void TestCovergare() throws InvalidSmilesException {
+        SmilesParser sp = new SmilesParser(DefaultChemObjectBuilder.getInstance());
+        IAtomContainer query = sp.parseSmiles("CCOC(=O)c1[nH]c2ccc(C)cc2(c1(N))");
+        IAtomContainer target = sp.parseSmiles("CCOC(=O)c2[nH]c1ccc(C)cc1c2(N=CN(CC)CC)");
+        if (query.getAtomCount() <= target.getAtomCount()) {
+            VF2 matcher = new VF2();
+            AtomMapping mapping = matcher.isomorphism(query, target);
+            System.out.println("mapping " + mapping);
+        }
+    }
+}
